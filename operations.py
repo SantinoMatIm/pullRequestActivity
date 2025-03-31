091ccdbeb23192ad354a9c45ca720c3e7088a3ae
“Leonardo <leordzglvz@gmail.com>
2025-03-31 12:40:45 -0600

    hice el mas y el menos

---
+++ operations.py
@@ -0,0 +1,11 @@
+a = int(input(“Give me the first number:”))
+b = int(input(“Give me the second number:”))
+
+# Display the addition 
+a+b
+
+# Display the subtraction
+a-b
+
+# Display multiplication
+
